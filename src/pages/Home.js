c81ada7b25c3891a578fa9ff6fddabe8fddc37c0
Mauricio-Guerrero-Arciniegas <maurizioguerrero@hotmail.com>
2022-06-03 20:25:24 -0500

    ecommerce1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,32 @@
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom"
 import { getProducts } from "../store/slices/products.slice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Card } from "react-bootstrap";
 
 const Home = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const products = useSelector(state => state.products);
 
   useEffect(() => {
     dispatch(getProducts());
   }, []);
 
+  console.log(products)
+
   return (
     <div>
       <h1>Home</h1>
+      {
+        products.map(productsItem => (
+          <Card onClick={() => navigate(`/products/${productsItem.id}`)}>
+            {productsItem.title}
+            <img src={productsItem.productImgs[0]}/>
+          </Card>
+        ))
+      }
     </div>
   );
 };
